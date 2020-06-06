39cf82303e804f6fb9af38210bc36613fb497737
mbrsagor <mbrsagor@gmail.com>
2020-06-07 00:21:34 +0600

    Backend API created

---
+++ contagion/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.contrib.auth.models import User
+from .helpers.enums import *
+
+
+class BaseEntity(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_at = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Country(BaseEntity):
+    name = models.CharField(max_length=95)
+    flag = models.ImageField(upload_to='flag/%y/%m', null=True, blank=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, default=None)
+    is_active = models.BooleanField(default=False)
+    order = models.IntegerField(blank=True, null=True, default=9999)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.name
+
+    def get_children(self):
+        return Country.objects.filter(parent=self)
+
+    def children_count(self):
+        return Country.objects.filter(parent=self).count()
+
+
+class Profile(BaseEntity):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField()
+    photo = models.ImageField(upload_to='profile/%Y/%m/', null=True)
+    date_of_birth = models.DateField(verbose_name="Birth Date", null=True)
+    gender = models.CharField(choices=GenderEnum.choices(), default=GenderEnum.MALE.value, max_length=16)
+    mobile = models.CharField(max_length=16, unique=True, null=True, blank=False)
+    address = models.CharField(max_length=95, null=True, blank=True)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+    @property
+    def name(self):
+        try:
+            return self.user.username
+        except AttributeError:
+            return 'N/A'
+
+
+class Disease(BaseEntity):
+    disease_name = models.CharField(max_length=120)
+    disease_image = models.ImageField(upload_to='disease/%y/%m', null=True, blank=True)
+
+    def __str__(self):
+        return self.disease_name
+
+
+class Contagion(BaseEntity):
+    disease = models.ForeignKey(Disease, on_delete=models.SET_NULL, null=True, related_name='ContagionName')
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, null=True, related_name='CountryName')
+    reporter = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='author')
+    daily_test = models.IntegerField(default=0)
+    daily_effected = models.IntegerField(default=0)
+    daily_dies = models.IntegerField(default=0)
+    daily_recovery = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.disease.disease_name
---
+++ contagion/views.py
@@ -0,0 +1,87 @@
+from .models import *
+from .serializers import *
+
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_200_OK, HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST
+from rest_framework.viewsets import ModelViewSet
+
+
+class CountryApiView(APIView):
+
+    def get(self, request):
+        queryset = Country.objects.all()
+        serializer = CountrySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CountrySerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, HTTP_400_BAD_REQUEST)
+
+
+class CountryUpdateDeleteView(APIView):
+
+    def put(self, request, id):
+        queryset = get_object_or_404(Country, id=id)
+        serializer = CountrySerializer(queryset, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        queryset = get_object_or_404(Country, id=id)
+        queryset.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
+
+
+class DiseaseViewSet(ModelViewSet):
+    queryset = Disease.objects.all()
+    serializer_class = DiseaseSerializer
+
+
+class ContagionApiView(APIView):
+
+    def get(self, request):
+        queryset = Contagion.objects.all()
+        serializer = ContagionSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ContagionSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+class ContagionUpdateDeleteView(APIView):
+
+    def get(self, request, id):
+        queryset = get_object_or_404(Contagion, id=id)
+        serializer = ContagionSerializer(queryset).data
+        return Response(serializer, status=HTTP_200_OK)
+
+    def put(self, request, id):
+        queryset = get_object_or_404(Contagion, id=id)
+        serializer = ContagionSerializer(queryset, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=HTTP_204_NO_CONTENT)
+
+    def delete(self, request, id):
+        queryset = get_object_or_404(Contagion, id=id)
+        queryset.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
