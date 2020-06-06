da84ab7ce64ef1d58d3d6a440e1d4912c1d898f8
mbrsagor <mbrsagor@gmail.com>
2020-06-07 00:24:39 +0600

    Django default admin panle added functionality

--- contagion/admin.py
+++ contagion/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+class CountryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name', 'parent', 'is_active',
+        'order', 'created_at', 'updated_at'
+    )
+
+    list_display_links = ['name']
+
+    list_filter = [
+        'name', 'parent', 'order'
+    ]
+
+    list_editable = ['order']
+    list_per_page = 6
+
+
+admin.site.register(Country, CountryAdmin)
+
+
+class DiseaseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'disease_name', 'created_at', 'updated_at')
+
+
+admin.site.register(Disease, DiseaseAdmin)
+
+
+class ContagionAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'disease', 'country', 'reporter',
+        'daily_test', 'daily_effected', 'daily_dies',
+        'daily_recovery', 'created_at', 'updated_at'
+    )
+
+    list_display_links = (
+        'disease', 'country', 'daily_effected'
+    )
+
+    search_fields = ['daily_test', 'daily_effected', 'daily_dies']
+    list_editable = ['daily_test', 'daily_dies', 'daily_recovery']
+    list_per_page = 6
+    list_filter = ['daily_test', 'daily_effected', 'daily_dies']
+
+
+admin.site.register(Contagion, ContagionAdmin)
