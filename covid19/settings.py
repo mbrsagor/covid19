6f760b16b7ef79d018a8bef75bb564f50f401753
mbrsagor <mbrsagor@gmail.com>
2020-07-03 23:39:14 +0600

    SMTP setup and config

--- covid19/settings.py
+++ covid19/settings.py
@@ -150,3 +150,10 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://localhost:8000'
 ]
+
+# Mail using SMTP
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'sagor@gmail.com'
+EMAIL_HOST_PASSWORD = 'sagor123'
+EMAIL_PORT = 587
