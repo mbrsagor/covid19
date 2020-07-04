42c7138b11468eeefb32f84000af1652753cf31f
mbrsagor <mbrsagor@gmail.com>
2020-07-04 21:26:25 +0600

    Local timezone added

--- config/db_config.py
+++ config/db_config.py
@@ -1,1 +1,1 @@
-DATABASES = {    'default': {        'ENGINE': 'django.db.backends.mysql',        'NAME': 'covid19',        'USER': 'root',        'PASSWORD': '12345678',        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on        'PORT': '3306',    }}
+DATABASES = {    'default': {        'ENGINE': 'django.db.backends.mysql',        'NAME': 'covid19',        'USER': 'root',        'PASSWORD': '12345678',        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on        'PORT': '3306',    }}
