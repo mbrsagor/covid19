72669b9f2fe976d6b9adad61fa36b6a294453ee0
Sagor <mbrsagor@gmail.com>
2020-08-10 10:21:49 +0600

    Remove default TokenAuthentication permission

--- covid19/settings.py
+++ covid19/settings.py
@@ -53,9 +53,9 @@ MIDDLEWARE = [
 # REST FRAMEWORK SETTINGS
 """
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    ),
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'rest_framework.authentication.TokenAuthentication',
+    # ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     )
