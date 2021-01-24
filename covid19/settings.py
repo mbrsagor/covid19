9e1108937edb5950658ede7b93710cbd86314457
Sagor <mbrsagor@gmail.com>
2021-01-25 02:56:00 +0600

    Rest framework default permisions, pagination, and JET allow

--- covid19/settings.py
+++ covid19/settings.py
@@ -118,13 +118,23 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-# Rest Framework config
+# REST FRAMEWORK SETTINGS
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+    'DATETIME_FORMAT': '%Y-%m-%d %I:%M %p ',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'PAGINATE_BY_PARAM': 'page_size'
 }
-
 
 # Rest Registration
 REST_REGISTRATION = {
--- hospital/views/appointment_views.py
+++ hospital/views/appointment_views.py
@@ -2,8 +2,10 @@ from hospital.models.appointment import Appointment
 from hospital.serializers.appointment_serializer import AppointmentSerializer
 
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 
 class AppointmentViewSet(ModelViewSet):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
+    permission_classes = [IsAuthenticated, ]
