c50740b0eda120e09ce743540b943d44d1804779
mbrsagor <mbrsagor@gmail.com>
2021-10-04 09:39:46 +0600

    Improved: Unsued files and codes removed

--- covid19/urls.py
+++ covid19/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/accounts/', include('rest_registration.api.urls')),
-
 ]
 
 if settings.DEBUG:
--- hospital/views/management_views.py
+++ hospital/views/management_views.py
@@ -1,1 +1,38 @@
-from hospital.models.management import *from hospital.serializers.management_serializer import *from hospital.permissions.permissions import PermissionHelperMixinfrom rest_framework.viewsets import ModelViewSetfrom rest_framework.permissions import IsAdminUserclass ExperienceViewSet(ModelViewSet, PermissionHelperMixin):    queryset = Experience.objects.all()    serializer_class = ExperienceSerializer    def get_permissions(self):        return self.admin_editable_only()class AvailabilityViewSet(ModelViewSet):    queryset = Availability.objects.all()    serializer_class = AvailabilitySerializer    # permission_classes = (IsAdminUser, )class LaboratoriesViewSet(ModelViewSet):    queryset = Laboratories.objects.all()    serializer_class = LaboratoriesSerializer    # permission_classes = (IsAdminUser,)class DepartmentViewSet(ModelViewSet):    queryset = Department.objects.all()    serializer_class = DepartmentSerializer    # permission_classes = (IsAdminUser,)class ServiceViewSet(ModelViewSet):    queryset = Service.objects.all()    serializer_class = ServiceSerializer    # permission_classes = (IsAdminUser,)
+from hospital.models.management import *
+from hospital.serializers.management_serializer import *
+from hospital.permissions.permissions import PermissionHelperMixin
+
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
+
+
+class ExperienceViewSet(ModelViewSet, PermissionHelperMixin):
+    queryset = Experience.objects.all()
+    serializer_class = ExperienceSerializer
+
+    def get_permissions(self):
+        return self.admin_editable_only()
+
+
+class AvailabilityViewSet(ModelViewSet):
+    queryset = Availability.objects.all()
+    serializer_class = AvailabilitySerializer
+    permission_classes = (IsAdminUser, )
+
+
+class LaboratoriesViewSet(ModelViewSet):
+    queryset = Laboratories.objects.all()
+    serializer_class = LaboratoriesSerializer
+    permission_classes = (IsAdminUser,)
+
+
+class DepartmentViewSet(ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+    # permission_classes = (IsAdminUser,)
+
+
+class ServiceViewSet(ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    # permission_classes = (IsAdminUser,)
