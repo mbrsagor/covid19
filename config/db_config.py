c5c87c68cb895b98fb20e1cbbbdcb1b1b963c3ab
mbrsagor <mbrsagor@gmail.com>
2021-10-21 15:00:47 +0600

    Remove: Delete config files

--- config/db_config.py
+++
@@ -1,1 +0,0 @@
-# Database# https://docs.djangoproject.com/en/3.1/ref/settings/#databasesDATABASES = {    'default': {        'ENGINE': 'django.db.backends.postgresql_psycopg2',        'NAME': 'covid19',        'USER': 'macair',        'PASSWORD': '12345',        'HOST': 'localhost',        'PORT': '',    }}
