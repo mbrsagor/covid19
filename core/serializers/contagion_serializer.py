d489b70b053a49eeb1cbe47593248db7ea750def
mbrsagor <mbrsagor@gmail.com>
2022-12-28 22:48:08 +0600

    New: validate name in location serializer.

--- core/serializers/contagion_serializer.py
+++ core/serializers/contagion_serializer.py
@@ -11,6 +11,11 @@ class LocationSerializer(ModelSerializer):
             'is_active', 'created_at', 'updated_at'
         ]
 
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError('Name must have al lest 3 charters.')
+        return value
+
 
 class UserSerializer(ModelSerializer):
     class Meta:
